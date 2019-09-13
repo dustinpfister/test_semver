e345c95d093b838a83556fa92d666be147fdebc4
dustinpfister <dustin.pfister@gmail.com>
2019-09-13 17:13:34 -0400

    bump script

--- demos/bump_1.js
+++ demos/bump_1.js
@@ -1,17 +1,20 @@
 let semver = require('semver'),
 path_packJSON = process.argv[3] || '../package.json',
+fs = require('fs'),
 pack = require(path_packJSON),
 release = process.argv[2] || 'patch',
 
 v = semver.valid(pack.version);
 
 if (v) {
-
-    v = semver.inc(v, release);
-    console.log(v);
-
+    pack.version = semver.inc(v, release);
+    fs.writeFile(path_packJSON, JSON.stringify(pack), (e) => {
+        if (e) {
+            console.log(e.message);
+        } else {
+            console.log('package.json version updated to ' + pack.version);
+        }
+    })
 } else {
-
     console.log('package.json does not contain a valid version number');
-
 }
